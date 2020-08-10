61b8e63995c9751bce9983c2f3bd98b3dc971169
Prajol Shakya <shakya_prajol@hotmail.com>
2020-08-10 16:35:07 -0500

    Updates

---
+++ precache-manifest.f8a898012de8caf50bf3a4326676652d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39495df9c64ca7f44ce5ac758338eb2",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "f78e1c82893246023369",
+    "url": "/react-calc/static/css/main.1a6f8731.chunk.css"
+  },
+  {
+    "revision": "c8297c366b49d22e823d",
+    "url": "/react-calc/static/js/2.aaa74b3b.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/react-calc/static/js/2.aaa74b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78e1c82893246023369",
+    "url": "/react-calc/static/js/main.bb3a142f.chunk.js"
+  },
+  {
+    "revision": "316ae6202d6dd97efa2a",
+    "url": "/react-calc/static/js/runtime-main.ef11baa1.js"
+  }
+]);
